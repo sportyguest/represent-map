252ce505083b0169a4be52a7cf1744c6c6f2ef33
Juan Mompeán Esteban <mompes@gmail.com>
2013-10-02 18:42:17 +0200

    Added twitter card and a screenshot of the map for twitter cards.

--- represent-map/index.php
+++ represent-map/index.php
@@ -2,7 +2,7 @@
 include_once "header.php";
 define('WP_USE_THEMES', false);
 //require_once( $_SERVER['DOCUMENT_ROOT'] . '/desarrollo/wp-blog-header.php' );
-require_once("/var/www/sportyguest/wp-blog-header.php");
+require_once("/var/www/sportyguest/wp-load.php");
 
 require_once ("include/evento.php");
 ?>
@@ -20,15 +20,17 @@ require_once ("include/evento.php");
     https://github.com/abenzer/represent-map
     -->
     <title>Mapa de eventos deportivos Sportyguest</title>
+    <meta charset="UTF-8">
     <meta name="description" content="Mapa de eventos deportivos con el que podrás planificar tu calendario deportivo con deportes como maratón, ciclismo, esquí, carreras populares, zonas de escalada o running.">
     <link rel="publisher" href="https://plus.google.com/s/sportyguest"/>
-    <meta name="twitter:card" content="summary">
-    <meta name="twitter:url" content="http://eventosdeportivos.sportyguest.es">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@sportyguest">
     <meta name="twitter:title" content="Mapa de eventos deportivos">
-    <meta name="twitter:image" content="http://www.sportyguest.es/mapa/represent-map/represent-map/images/badges/badge2.png">
     <meta name="twitter:description" content="Mapa de eventos deportivos con el que podrás planificar tu calendario deportivo con deportes como maratón, ciclismo, esquí, carreras populares, zonas de escalada o running.">
+    <meta name="twitter:creator" content="@sportyguest">
+    <meta name="twitter:image:src" content="http://carreraspopulares.sportyguest.es/images/preview.png">
+    <meta name="twitter:domain" content="http://carreraspopulares.sportyguest.es/">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <meta charset="UTF-8">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700|Open+Sans:400,700' rel='stylesheet' type='text/css'>
     <link href="./bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" />
     <link href="./bootstrap/css/bootstrap-responsive.css" rel="stylesheet" type="text/css" />

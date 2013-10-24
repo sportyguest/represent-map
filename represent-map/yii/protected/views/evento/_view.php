71b36f0b8ea235a01a1d135cae0e68f4d63221f0
Juan Mompeán Esteban <mompes@gmail.com>
2013-10-24 11:46:07 +0200

    Added yii framework and an API for the events.

---
+++ represent-map/yii/protected/views/evento/_view.php
@@ -0,0 +1,71 @@
+<?php
+/* @var $this EventoController */
+/* @var $data Evento */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('owner_name')); ?>:</b>
+	<?php echo CHtml::encode($data->owner_name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('owner_email')); ?>:</b>
+	<?php echo CHtml::encode($data->owner_email); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
+	<?php echo CHtml::encode($data->name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('image_url')); ?>:</b>
+	<?php echo CHtml::encode($data->image_url); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('description')); ?>:</b>
+	<?php echo CHtml::encode($data->description); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('url')); ?>:</b>
+	<?php echo CHtml::encode($data->url); ?>
+	<br />
+
+	<?php /*
+	<b><?php echo CHtml::encode($data->getAttributeLabel('address')); ?>:</b>
+	<?php echo CHtml::encode($data->address); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('lat')); ?>:</b>
+	<?php echo CHtml::encode($data->lat); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('lng')); ?>:</b>
+	<?php echo CHtml::encode($data->lng); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('category')); ?>:</b>
+	<?php echo CHtml::encode($data->category); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('subcategory')); ?>:</b>
+	<?php echo CHtml::encode($data->subcategory); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('creation_date')); ?>:</b>
+	<?php echo CHtml::encode($data->creation_date); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('date')); ?>:</b>
+	<?php echo CHtml::encode($data->date); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('approved')); ?>:</b>
+	<?php echo CHtml::encode($data->approved); ?>
+	<br />
+
+	*/ ?>
+
+</div>

dae3e4e59b2af72a3f2d9712e1e8e775f0dfd616
Juan Mompeán Esteban <mompes@gmail.com>
2013-11-11 13:24:34 +0100

    Added missing fields to facebook action "rate".

--- represent-map/yii/protected/views/eventoValoracion/view.php
+++ represent-map/yii/protected/views/eventoValoracion/view.php
@@ -14,8 +14,18 @@ $this->menu=array(
 	array('label'=>'Delete EventoValoracion', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
 	array('label'=>'Manage EventoValoracion', 'url'=>array('admin')),
 );
+$evento = Evento::model()->find(array(
+    'select'=>'title,description,image_url',
+    'condition'=>'id=:id',
+    'params'=>array(':id'=>$model->evento_id),
+));
 Yii::app()->clientScript->registerMetaTag("167839766714035", null, null, array('property'=>'og:app_id'), null);
 Yii::app()->clientScript->registerMetaTag("sportyguest_eventos:rating", null, null, array('property'=> 'og:type'), null);
+Yii::app()->clientScript->registerMetaTag($evento->title, null, null, array('property' => 'og:title'), null);
+Yii::app()->clientScript->registerMetaTag($evento->description, null, null, array('property' => 'og:description'), null);
+if (!empty($evento->image_url)) {
+	Yii::app()->clientScript->registerMetaTag($evento->image_url, null, null, array('property'=> 'og:image'), null);
+}
 Yii::app()->clientScript->registerMetaTag($model->valoracion, null, null, array('property'=> 'sportyguest_eventos:rating_value'), null);
 Yii::app()->clientScript->registerMetaTag(EventoValoracion::VALORACION_ESCALA, null, null, array('property'=> 'sportyguest_eventos:rating_scale'), null);
 Yii::app()->clientScript->registerMetaTag($model->valoracion_organizacion, null, null, array('property'=> 'sportyguest_eventos:rating_org_value'), null);

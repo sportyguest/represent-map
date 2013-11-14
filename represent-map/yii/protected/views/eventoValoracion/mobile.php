4c5d7f297c3a27b5806182b8f99f8a4ac9cb5b22
Juan Mompeán Esteban <mompes@gmail.com>
2013-11-14 12:17:03 +0100

    Added a layout so Yii knows how to manage correctly the page.

--- represent-map/yii/protected/views/eventoValoracion/mobile.php
+++ represent-map/yii/protected/views/eventoValoracion/mobile.php
@@ -1,131 +1,115 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-  <meta name="apple-mobile-web-app-capable" content="yes">
-  <meta name="apple-mobile-web-app-status-bar-style" content="black">
-  <title>Valora el evento</title>
-  
-  
-  
-  <link rel="stylesheet" href="https://d10ajoocuyu32n.cloudfront.net/mobile/1.3.1/jquery.mobile-1.3.1.min.css">
-  
-  <!-- jQuery and jQuery Mobile -->
-  <script src="https://d10ajoocuyu32n.cloudfront.net/jquery-1.9.1.min.js"></script>
-  <script src="https://d10ajoocuyu32n.cloudfront.net/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>
-   
-</head>
-<body>
-    <!-- Load the Facebook JavaScript SDK -->
-    <div id="fb-root"></div>
-    <script src="http://connect.facebook.net/en_US/all.js"></script>
+<?php
+$this->layout = "mobile_rating_layout";
+?>
 
-    <script type="text/javascript">
-      var url_home = "http://eventosdeportivos.sportyguest.es/";
-      // Initialize the Facebook JavaScript SDK
-      FB.init({
-        appId: '167652430078861',
-        xfbml: true,
-        status: true,
-        cookie: true,
-      });
+<!-- Load the Facebook JavaScript SDK -->
+<div id="fb-root"></div>
+<script src="http://connect.facebook.net/en_US/all.js"></script>
 
-      $(document).ready(function() {
-        // Hide emails specific fields
-        $("#comment-group").hide();
-        $("#email-group").hide();
-        // Facebook button clicked
-        $("#send-fb").click(function() {
-          var evento_id = $("#evento").val();
-          var val_general = $("#val_general").val();
-          var val_organization = $("#val_organization").val();
-          var val_difficulty = $("#val_difficulty").val();
-          var val_route = $("#val_route").val();
-          var val_extra_activities = $("#val_extra_activities").val();
-          var val_price = $("#val_price").val();
-          FB.login(function(response) {
-            if(response && response.status == 'connected') {
-              valorar(evento_id, response.authResponse.userID);
-            } 
-          }, {scope:'email,publish_actions'});
-        });
-        // Email button clicked
-        $("#send-email").click(function() {
-          console.log("Clicado send email");
-          if ($("#email").is(":visible") && $("#email").val()) {
-            console.log("Campo email es visible");
-            var evento_id = $("#evento").val();
-            var val_general = $("#val_general").val();
-            var val_organization = $("#val_organization").val();
-            var val_difficulty = $("#val_difficulty").val();
-            var val_route = $("#val_route").val();
-            var val_extra_activities = $("#val_extra_activities").val();
-            var val_price = $("#val_price").val();
-            valorar(evento_id, 0);
-          } else {
-            console.log("Campo email no es visible");
-            $("#email-group").show();
-            $("#comment-group").show();
-          }
-        });
-      });
-      function valorar(evento_id, uid) {
-        var rating = jQuery("#val_general").val();
-        var rating_org = jQuery("#val_organization").val();
-        var rating_diff = jQuery("#val_difficulty").val();
-        var rating_route = jQuery("#val_route").val();
-        var rating_price = jQuery("#val_price").val();
-        var rating_extra = jQuery("#val_extra_activities").val();
-        if (!uid) {
-          var comment = jQuery("#comment").val();
-          var email = jQuery("#email").val();
-        }
-        var data = {
-          'EventoValoracion[evento_id]': evento_id, 
-          'EventoValoracion[facebook_id]': uid, 
-          'EventoValoracion[valoracion]': rating,
-          'EventoValoracion[valoracion_organizacion]': rating_org,
-          'EventoValoracion[valoracion_dificultad]': rating_diff,
-          'EventoValoracion[valoracion_recorrido]': rating_route,
-          'EventoValoracion[valoracion_precio]': rating_price,
-          'EventoValoracion[valoracion_actividad_complementaria]': rating_extra,
-          'EventoValoracion[comentario]': comment,
-          'EventoValoracion[email]': email
-        };
-        // The rating is created in the database and then the rating is post to facebook
-        $.ajax({
-          type: 'POST',
-          url: url_home + 'yii/eventoValoracion/ajax',
-          data: data,
-          success:function(data){
-            console.log(data);
-            if (uid != 0 && data.code == "success") {
-              var rating_url = url_home + 'yii/eventoValoracion/view/id/' + data.id;
-              FB.api(
-                'me/sportyguest_eventos:rating',
-                'post',
-                {
-                  'rating': rating_url,
-                  'sport_event': url_home + 'yii/evento/view/id/' + evento_id
-                },
-                function(response) {
-                  alert("Gracias por valorar");
-                  window.location.href = url_home + "v/comentario.php?evento_id=" + evento_id;
-                }
-              );
-            } else {
-              window.location.href = url_home + "v/gracias.html";
-            }
-          },
-          error: function(data) { // if error occured
-            alert("Error occured.please try again");
-            console.log(data);
-          },
-          dataType:'json'
-        });
+<script type="text/javascript">
+  var url_home = "http://eventosdeportivos.sportyguest.es/";
+  // Initialize the Facebook JavaScript SDK
+  FB.init({
+    appId: '167652430078861',
+    xfbml: true,
+    status: true,
+    cookie: true,
+  });
+
+  $(document).ready(function() {
+    // Hide emails specific fields
+    $("#comment-group").hide();
+    $("#email-group").hide();
+    // Facebook button clicked
+    $("#send-fb").click(function() {
+      var evento_id = $("#evento").val();
+      var val_general = $("#val_general").val();
+      var val_organization = $("#val_organization").val();
+      var val_difficulty = $("#val_difficulty").val();
+      var val_route = $("#val_route").val();
+      var val_extra_activities = $("#val_extra_activities").val();
+      var val_price = $("#val_price").val();
+      FB.login(function(response) {
+        if(response && response.status == 'connected') {
+          valorar(evento_id, response.authResponse.userID);
+        } 
+      }, {scope:'email,publish_actions'});
+    });
+    // Email button clicked
+    $("#send-email").click(function() {
+      console.log("Clicado send email");
+      if ($("#email").is(":visible") && $("#email").val()) {
+        console.log("Campo email es visible");
+        var evento_id = $("#evento").val();
+        var val_general = $("#val_general").val();
+        var val_organization = $("#val_organization").val();
+        var val_difficulty = $("#val_difficulty").val();
+        var val_route = $("#val_route").val();
+        var val_extra_activities = $("#val_extra_activities").val();
+        var val_price = $("#val_price").val();
+        valorar(evento_id, 0);
+      } else {
+        console.log("Campo email no es visible");
+        $("#email-group").show();
+        $("#comment-group").show();
       }
-    </script>
+    });
+  });
+  function valorar(evento_id, uid) {
+    var rating = jQuery("#val_general").val();
+    var rating_org = jQuery("#val_organization").val();
+    var rating_diff = jQuery("#val_difficulty").val();
+    var rating_route = jQuery("#val_route").val();
+    var rating_price = jQuery("#val_price").val();
+    var rating_extra = jQuery("#val_extra_activities").val();
+    if (!uid) {
+      var comment = jQuery("#comment").val();
+      var email = jQuery("#email").val();
+    }
+    var data = {
+      'EventoValoracion[evento_id]': evento_id, 
+      'EventoValoracion[facebook_id]': uid, 
+      'EventoValoracion[valoracion]': rating,
+      'EventoValoracion[valoracion_organizacion]': rating_org,
+      'EventoValoracion[valoracion_dificultad]': rating_diff,
+      'EventoValoracion[valoracion_recorrido]': rating_route,
+      'EventoValoracion[valoracion_precio]': rating_price,
+      'EventoValoracion[valoracion_actividad_complementaria]': rating_extra,
+      'EventoValoracion[comentario]': comment,
+      'EventoValoracion[email]': email
+    };
+    // The rating is created in the database and then the rating is post to facebook
+    $.ajax({
+      type: 'POST',
+      url: url_home + 'yii/eventoValoracion/ajax',
+      data: data,
+      success:function(data){
+        console.log(data);
+        if (uid != 0 && data.code == "success") {
+          var rating_url = url_home + 'yii/eventoValoracion/view/id/' + data.id;
+          FB.api(
+            'me/sportyguest_eventos:rating',
+            'post',
+            {
+              'rating': rating_url,
+              'sport_event': url_home + 'yii/evento/view/id/' + evento_id
+            },
+            function(response) {
+              window.location.href = url_home + "v/comentario.php?evento_id=" + evento_id;
+            }
+          );
+        } else {
+          window.location.href = url_home + "v/gracias.html";
+        }
+      },
+      error: function(data) { // if error occured
+        alert("Ha ocurrido un error, inténtelo de nuevo por favor.");
+        console.log(data);
+      },
+      dataType:'json'
+    });
+  }
+</script>
 <!-- Home -->
 <div data-role="page" id="page1">
     <div data-role="content">
@@ -133,7 +117,7 @@
             <img style="width: 288px; height: 100px" src="http://eventosdeportivos.sportyguest.es/images/badges/badge1.png">
         </div>
         <h1>
-            Puntúa el evento
+            Valora el evento
         </h1>
         <div data-role="fieldcontain">
             <label for="evento">
@@ -205,5 +189,3 @@
         </a>
     </div>
 </div>
-</body>
-</html>
---
+++ represent-map/yii/protected/views/layouts/mobile_rating_layout.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <title>Valora el evento</title>
+  
+  
+  
+  <link rel="stylesheet" href="https://d10ajoocuyu32n.cloudfront.net/mobile/1.3.1/jquery.mobile-1.3.1.min.css">
+  
+  <!-- jQuery and jQuery Mobile -->
+  <script src="https://d10ajoocuyu32n.cloudfront.net/jquery-1.9.1.min.js"></script>
+  <script src="https://d10ajoocuyu32n.cloudfront.net/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>
+   
+</head>
+<body>
+	
+	<?php echo $content; ?>
+
+</body>
+</html>
